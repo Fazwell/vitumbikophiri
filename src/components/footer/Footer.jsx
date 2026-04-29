3f92f090a9487b6933d04f69dac0e557314f89b6
Fazwell <mwangufaz@gmail.com>
2026-04-29 21:23:17 +0200

    changed some colors

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
             <img src={personalLogo} alt='PRG' />
             </div>
             
-            <h1 className='footer__title'>Fazwell</h1>
+            <h1 className='footer__title'>Vitumbiko Phiri</h1>
             <ul className='footer__list'>
                 <li>
                     <a href="#about" className="footer__link">About</a>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header className="header">
         <nav className="nav container">
-            <a href="index.html" className="nav__logo">Fazwell</a>
+            <a href="index.html" className="nav__logo">Vitumbiko Phiri</a>
             <div className={Toggle ? 'nav__menu show-menu': 'nav__menu'}>
                 <ul className="nav__list grid">
                     <li className='nav__item'>
